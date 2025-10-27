ca698a1c2342828c21b94d517f7368da5a954adb
JohanVillazar <villazarjohan@gmail.com>
2025-10-27 09:27:40 -0500

    updateerror

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,11 +4,7 @@ import "./featured.scss";
 
 const Featured = () => {
   const { data, loading, error } = useFetch(
-<<<<<<< HEAD
     "https://magictravel.onrender.com/api/hotels/countByCity?cities=bucaramanga,bogota,medellin,cartagena,santa marta,manizales,chicamocha,cali"
-=======
-    "https://magictravel.onrender.comapi/hotels/countByCity?cities=bucaramanga,bogota,medellin,cartagena,santa marta,manizales,chicamocha,cali"
->>>>>>> 796f75a (magictravelfront)
   );
   const navigate = useNavigate();
 
